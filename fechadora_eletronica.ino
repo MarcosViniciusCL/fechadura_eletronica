85a96bdc3771a65296e0b9a0ab91ea4abc3c3d3e
Marcos Vinicius <marcosviniciuscl@outlook.com>
2019-07-27 19:50:25 -0300

    Update fechadora_eletronica.ino

--- fechadora_eletronica.ino
+++ fechadora_eletronica.ino
@@ -1,220 +1,177 @@
+#include <SPI.h>
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include "WifiESP.h"
+//#include "MQTT.h"
+#include "RFID.h"
 #include <EEPROM.h>
-#include <user_interface.h>
+#include <PubSubClient.h>
 
-#define DEBUG
+#define SS_PIN D4
+#define RST_PIN D2
+
+#define VECTOR_SIZE_MAX 30
+String secure_card[VECTOR_SIZE_MAX];
+int counter_master_card=0;
+int index_card=0;
+int _look=0;
+
+#define TRY_MAX 5
+int _try=0;
+
+
+
 #define LED_NOT_VERDE 2
-#define LED_NOT_VERMELHO 3
-
-//informações do broker MQTT - Verifique as informações geradas pelo CloudMQTT
-
-#define BOOTING  0x00 // Boot
-#define CONNECTING_WIFI  0x01 // Conectando ao wifi
-#define CONNECTED_WIFI   0x02 // Conectado ao wifi
-#define CONNECTING_MQTT  0x03 // Conectando ao broker MQtt
-#define CONNECTED_MQTT   0x04 // Conectado ao wifi
-#define READY  0x05 // Pronto para uso
-#define BRIDGE  0x06 // Estado NULL, entre um estado e outro
-
-
-
-//Erros
-#define E_S10 0x10 // Rede wifi não encontrada
-#define E_S20 0x20 // Sem conexão com Broker
-
-
-os_timer_t tmr0;
-int counter = 0;
-char state = 0x00; //Variavel para estado do sistema
+#define LED_NOT_VERMELHO D3
+#define RELE D1
+#define BUZZER D3
 
 // flags
-bool __config_mode=false;
-bool __status=false;
+bool __config_mode=false;                                   // Flag para entra no modo de configuração
+bool __status=false;                                        // Flag usada para habilitar a exibição de status
+bool __reset=false;                                         // Flag para um reset no microcontrolador
 
-typedef struct ConfigWifi {
-  String ssid;
-  String password;
-} ConfigWifi;
+bool __save_new_card=false;
 
-typedef struct {
+typedef struct m {
   String server;
-  int port;
   String user;
   String password;
-  String topicSub;
-} ConfigMqtt;
-
-ConfigWifi configWifi;
-ConfigMqtt configMqtt;
+  int port;
+  String subTopic;
+} conf_mqtt; 
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+conf_mqtt mqtt;
 
-void setup() {
- // EEPROM.begin(512);
-  // ------- Carregando configurações do wifi 
-  getConfigMqtt();
-  //configWifi.ssid = "NOTE";
-  //configWifi.password = "@n3tworking";
-  /*configMqtt.server = "postman.cloudmqtt.com";   
-  configMqtt.user = "jsxgzfyb";              
-  configMqtt.password = "kbnsDu-ZSXlS";      
-  configMqtt.port = 14157;                    
-  configMqtt.topicSub ="almiro_galeria/atuadores/porta_escada";            //tópico que sera ass*/
-   
-  os_timer_setfn(&tmr0, notif_usuario, NULL); //Indica ao Timer qual sera sua Sub rotina.
-  os_timer_arm(&tmr0, 100, true);  //Inidica ao Timer seu Tempo em mS e se sera repetido ou apenas uma vez (loop = true)
-                                     //Neste caso, queremos que o processo seja repetido, entao usaremos TRUE.
-  //state = getCurState();
-  Serial.begin(115200);
+WifiESP *wifi;
+RFID *rfid;
+
+char state;                                                 // O estado que se encontra o microcontrolador
+int counter;
+int counter_mqtt;
+
+os_timer_t tmr0;
+String tagAtual;
+
+
+
+void setup(){
+  Serial.begin(115200);                                     //Configurando a velocidade da uart
+  SPI.begin();                                              //Iniciando a interface SPI para usar o RFID    
+  //EEPROM.begin(32);
+
+  secure_card[0] = "146191131";
+  rfid = new RFID();
+  rfid->init();
+  
+  wifi = new WifiESP(&state);                               //Criando objeto para controle do wifi
+  //wifi->loadMemory();
+  wifi->setSSID("NOTE");                                    //Configurando SSID
+  wifi->setPassword("@n3tworking");                         //Configurando senha
+  WiFi.begin(wifi->getSSID(), wifi->getPassword());
+  //wifi->saveMemory();
+
   pinMode(LED_NOT_VERDE, OUTPUT);
-  conecte_wifi();
-  client.setServer(configMqtt.server.c_str(), configMqtt.port);
+  pinMode(RELE, OUTPUT);                                    // Informando que o pino do rele é uma saída
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(RELE, 1);                                    // Escrevendo nivel logico alto no pino do rele, o rele é acionado com nivel logico baixo
+
+  mqtt.server = "postman.cloudmqtt.com";
+  mqtt.user = "jsxgzfyb";
+  mqtt.password = "Gb5a2cgqb_K6";
+  mqtt.port = 14157;
+  mqtt.subTopic = "/galeria/portao/esp8266";
+  client.setServer(mqtt.server.c_str(), mqtt.port);
   client.setCallback(callback);
-  conecte_broker();
-  //subscreve no tópico
-  client.subscribe(configMqtt.topicSub.c_str());
-  //setCurState(READY);
+  //mqtt.saveMemory();
+  //mqtt.loadMemory();
+  
+  
+  os_timer_setfn(&tmr0, interrupt_time, NULL);              //Indica ao Timer qual sera sua Sub rotina.
+  os_timer_arm(&tmr0, 500, true);                           //Indica ao Timer seu Tempo em ms e se será repetido (loop = true)
+  counter = 0;
+  counter_mqtt = 0;
+  counter_master_card = 0;
+  buzzer(1000, 2);
 }
 
-void conecte_wifi(){
-  if(getCurState() != CONNECTING_WIFI){
-    WiFi.begin(configWifi.ssid, configWifi.password);
-    Serial.print(configWifi.ssid);
-    #ifdef DEBUG
-        Serial.println("\nConectando ao WiFi..");  
-    #endif
-  }
-  setCurState(CONNECTING_WIFI);
-  if (WiFi.status() != WL_CONNECTED) {
-    
-  }
-  if (WiFi.status() == WL_CONNECTED) {
-    #ifdef DEBUG
-    Serial.println("\nConectado na rede WiFi " + configWifi.ssid);
-    #endif
-  }
-  delay(100);
- 
-}
-
-void conecte_broker(){
-  if(WiFi.status() == WL_CONNECTED) {
-    setCurState(CONNECTING_MQTT);
-    if (!client.connected()) {
-      if (!isConnectWifi()) conecte_wifi();
-      #ifdef DEBUG
-      Serial.println("\nConectando ao Broker MQTT...");
-      #endif
-   
-      if (client.connect("ESP8266Client", configMqtt.user.c_str(), configMqtt.password.c_str())) {
-        setCurState(CONNECTED_MQTT);
-        #ifdef DEBUG
-        Serial.println("\nConectado");  
-        #endif
-   
-      } else {
-        #ifdef DEBUG 
-        Serial.print("falha estado  ");
-        Serial.print(client.state());
-        #endif
-        delay(2000);
-      }
+void connect_mqtt(){
+    if(WiFi.status() == WL_CONNECTED && counter_mqtt >= 1000) {
+        Serial.println("\nTentando conectar MQtt");
+        if (client.connect("ESP8266Client", mqtt.user.c_str(), mqtt.password.c_str())) {
+          Serial.println("\nConectado MQtt");
+          client.subscribe(mqtt.subTopic.c_str());
+        }
+        counter_mqtt = 0;
     }
-  }
-  //setCurState(BRIDGE);
+    counter_mqtt++;
+    if(counter_mqtt > 1000) counter_mqtt = 0;
 }
- 
+      
+
 void callback(char* topic, byte* payload, unsigned int length) {
-
-  //armazena msg recebida em uma sring
-  payload[length] = '\0';
-  String strMSG = String((char*)payload);
-
-  #ifdef DEBUG
-  Serial.print("Mensagem chegou do tópico: ");
-  Serial.println(topic);
-  Serial.print("Mensagem:");
-  Serial.print(strMSG);
-  Serial.println();
-  Serial.println("-----------------------");
-  #endif
-
-  //aciona saída conforme msg recebida 
-  if (strMSG == "1"){         //se msg "1"
-     digitalWrite(LED_NOT_VERDE, LOW);  //coloca saída em LOW para ligar a Lampada - > o módulo RELE usado tem acionamento invertido. Se necessário ajuste para o seu modulo
-  }else if (strMSG == "0"){   //se msg "0"
-     digitalWrite(LED_NOT_VERDE, HIGH);   //coloca saída em HIGH para desligar a Lampada - > o módulo RELE usado tem acionamento invertido. Se necessário ajuste para o seu modulo
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String msg = "";
+  for (int i = 0; i < length; i++) {
+    msg += (char)payload[i];
   }
- 
-}
 
-//função pra reconectar ao servido MQTT
-void reconect() {
-  if(WiFi.status() == WL_CONNECTED) {
-    //Enquanto estiver desconectado
-    setCurState(CONNECTING_MQTT);
-    if (!client.connected()) {
-      if (!isConnectWifi()){ conecte_wifi(); setCurState(CONNECTING_MQTT);}
-      #ifdef DEBUG
-      Serial.print("Tentando conectar ao servidor MQTT");
-      #endif
-       
-      bool conectado = strlen(configMqtt.user.c_str()) > 0 ? client.connect("ESP8266Client", configMqtt.user.c_str(), configMqtt.password.c_str()) : client.connect("ESP8266Client");
-  
-      if(conectado) {
-        #ifdef DEBUG
-        Serial.println("\nConectado!");
-        #endif
-        //subscreve no tópico
-        client.subscribe(configMqtt.topicSub.c_str(), 1); //nivel de qualidade: QoS 1
-      } else {
-        #ifdef DEBUG
-        Serial.print("Falha durante a conexão.Code: ");
-        Serial.println( String(client.state()).c_str());
-        #endif
-        //Aguarda 3 segundos 
-        delay(3000);
-      }
-    }
-    setCurState(READY);
+  // Switch on the LED if an 1 was received as first character
+  if (msg.equals("1")) {
+    acionarRele("mqtt");
+  } else if(msg.equals("add_new_card")){
+    add_new_card(false);
+  } else if(msg.equals("add_new_card master")){
+    add_new_card(true);
   }
-  
 }
 
-void verif_prim_execucao(){
-  EEPROM.read(0);
+void beepBuzzer(int delayMs){
+  digitalWrite(BUZZER, 1);
+  delay(delayMs);
+  digitalWrite(BUZZER, 0);
 }
 
-void notif_usuario(void* z){
+void buzzer(int delay_t, int co_unt){
+  int i = 0;
+  for(i = 0; i < co_unt; i++){
+    delay(delay_t);
+    digitalWrite(BUZZER, !digitalRead(BUZZER));
+  }
+  digitalWrite(BUZZER, 0);
+}
+
+void beepBuzzerErro(int delayMs){
+  digitalWrite(BUZZER, 1);
+  delay(delayMs);
+  digitalWrite(BUZZER, 0);
+  delay(delayMs);
+  digitalWrite(BUZZER, 1);
+  delay(delayMs);
+  digitalWrite(BUZZER, 0);
+  delay(delayMs);
+  digitalWrite(BUZZER, 1);
+  delay(delayMs);
+  digitalWrite(BUZZER, 0);
+}
+
+
+void notificacao_led(){
+  counter += 1;
+  if(counter%2 == 0){ // Acontece a cada 1 segundo
+    _look > 0 ? _look-- : _look=0;
+  }
+  if(counter >= 40){ // 20 Segundos
+    counter_master_card = 0;
+    counter = 0;
+  }
+}
+
+void interrupt_time(void* z){
   if(!__config_mode){
-    counter += 1;
-    switch(state){
-        case(CONNECTING_WIFI):
-          if(counter >= 1){ // 100 ms
-              counter = 0;
-              digitalWrite(LED_NOT_VERDE, !digitalRead(LED_NOT_VERDE));
-          }
-          break;
-        case(CONNECTING_MQTT):
-          if(counter >= 8){ // 1 segundo
-            counter = 0;
-            digitalWrite(LED_NOT_VERDE, !digitalRead(LED_NOT_VERDE));
-          }
-          break;
-        case(READY):
-          if(counter >= 30){ // 3 segundos
-            counter = 0;
-            digitalWrite(LED_NOT_VERDE, !digitalRead(LED_NOT_VERDE));
-          }
-          break;
-        case(BRIDGE):
-            digitalWrite(LED_NOT_VERDE, 1);
-          break;
-        default:
-          break;
-    }
     if(Serial.available() > 0){
       String c = "";
       while(Serial.available() > 0) c.concat((char)Serial.read());
@@ -227,53 +184,41 @@ void notif_usuario(void* z){
       if(c.equals("status\r\n")){
         __status=true;
       }
+      if(c.equals("reset\r\n")){
+        __reset=true;
+      }
     }
-  } else {
-    digitalWrite(LED_NOT_VERDE, 0);
+    notificacao_led();
   }
-}
-
-char getCurState(){
-  return state;
-}
-
-char setCurState(char value){
-  state = value;
-}
-
-
-// Apaga toda a EEPROM para um reset de dados. Comando deve ser recebido via MQTT.
-void resetAll(){
-  int l = EEPROM.length();
-  int i = 0;
-  for(i=0; i<l; i++){
-    EEPROM.write(i, 0x00);
-  }
-}
-
-//Verifica se o ESP ainda se encontra conectado ao WIFI
-bool isConnectWifi(){
-  int v = WiFi.status();
-  return !(v == WL_DISCONNECTED || v == WL_CONNECTION_LOST || v == WL_CONNECT_FAILED || v == 1 );
-}
-
-void openPort(){
   
+}
+
+String readSerial(){
+  String c;
+  while(Serial.available() <= 0){};
+  c = Serial.readString();
+  c.remove(c.length()-2, 2);
+  return c;
+}
+
+void reiniciar(){
+  printMsg("Reiniciando...");
+  //resetEsp();
 }
 
 void config_all(){
   String c;
   Serial.println("\n\n************************************************************");
   Serial.println("******************* MODO CONFIGURAÇÃO **********************");
-  Serial.println("************************************************************");
+  Serial.println("******* OBS: SISTEMA FICA PARADO NO MODO CONFIG ************");
   Serial.println("DIGITE UMA DAS OPÇÕES");
-  Serial.println("\nconfig_wifi: Entrar nas configurações do wifi");
-  Serial.println("\nconfig_mqtt: Entrar nas configurações do serviço MQTT");
+  Serial.println("\nwifi: Entrar nas configurações do wifi");
+  Serial.println("mqtt: Entrar nas configurações do serviço MQTT");
   c = readSerial();
-  if(c.equals("config_wifi")){
+  if(c.equals("wifi")){
     config_wifi();
   }
-  if(c.equals("config_mqtt")){
+  if(c.equals("mqtt")){
     config_mqtt();
   }
   __config_mode=false;
@@ -281,100 +226,182 @@ void config_all(){
 
 void config_wifi(){
   Serial.print("\nWIFI CONFIG\n");
-  Serial.print("SSID: " + String(configWifi.ssid));
-  Serial.print("\nPASSWORD: " + String(configWifi.password));
+  Serial.print("SSID: " + String(wifi->getSSID()));
+  Serial.print("\nPASSWORD: " + String(wifi->getPassword()));
   Serial.print("\nA - Alterar configurações\n");
   Serial.print("S - Sair\n");
   if(readSerial().equals("A")){
     Serial.print("Digite as opções\n");
     Serial.print("SSID: ");
-    configWifi.ssid = readSerial();
-    Serial.print(configWifi.ssid);
+    wifi->setSSID(readSerial());
+    Serial.print(wifi->getSSID());
     Serial.print("\nPASSWORD: ");
-    configWifi.password = readSerial();
-    Serial.print(configWifi.password);
+    wifi->setPassword(readSerial());
+    Serial.print(wifi->getPassword());
     Serial.print("\n\nConfigurações salvas!\n");
-    //saveConfigWifi();
-    conecte_wifi();
+    wifi->desconectar();
+    wifi->saveMemory();
+    //saveConfigWifi(); //Tem que desconectar e conectar novamente no wifi
   }
 }
 void config_mqtt(){
   Serial.print("\nMQTT CONFIG\n");
-  Serial.print("SERVER: " + String(configMqtt.server));
-  Serial.print("\nPORT: " + String(configMqtt.port));
-  Serial.print("\nUSER: " + String(configMqtt.user));
-  Serial.print("\nPASSWORD: " + String(configMqtt.password));
+  Serial.print("SERVER: " + String(mqtt.server));
+  Serial.print("\nPORT: " + String(mqtt.port));
+  Serial.print("\nUSER: " + String(mqtt.user));
+  Serial.print("\nPASSWORD: " + String(mqtt.password));
   Serial.print("\nA - Alterar configurações\n");
   Serial.print("S - Sair\n");
   if(readSerial().equals("A")){
     Serial.print("Digite as opções\n");
     Serial.print("\nSERVER: ");
-    configMqtt.server = readSerial();
-    Serial.print(configMqtt.server);
+    mqtt.server = readSerial();
+    Serial.print(mqtt.server);
 
     Serial.print("\nPORT: ");
-    configMqtt.port = readSerial().toInt();
-    Serial.print(configMqtt.port);
+    mqtt.port = readSerial().toInt();
+    Serial.print(mqtt.port);
     
     Serial.print("\nUSER: ");
-    configMqtt.user = readSerial();
-    Serial.print(configMqtt.user);
+    mqtt.user = readSerial();
+    Serial.print(mqtt.user);
     
     Serial.print("\nPASSWORD: ");
-    configMqtt.password = readSerial();
-    Serial.print(configMqtt.password);
+    mqtt.password = readSerial();
+    Serial.print(mqtt.password);
 
     Serial.print("\nTOPIC: ");
-    configMqtt.topicSub = readSerial();
-    Serial.print(configMqtt.topicSub);
+    mqtt.subTopic = readSerial();
+    Serial.print(mqtt.subTopic);
     
     Serial.print("\n\nConfigurações salvas!\n");
-    saveConfigMqtt();
-    conecte_broker();
+    //saveConfigMqtt();
+    //conecte_broker(); //Disconectar broker e conectar novamente
   } 
 }
 
 void status_all(){
   Serial.println("\n----------- STATUS -----------");
   Serial.print("\nWIFI: "); 
-  isConnectWifi() ? Serial.print("CONECTADO") : Serial.print("DESCONECTADO");
+  WiFi.status() == WL_CONNECTED ? Serial.print("CONECTADO") : Serial.print("DESCONECTADO");
   Serial.print("\nMQtt: "); 
   client.connected() ? Serial.print("CONECTADO\n") : Serial.print("DESCONECTADO\n");
   Serial.print("\nAcess Point: "); 
-  Serial.print((char*)WiFi.BSSID());
+  Serial.print(wifi->getSSID());
+  Serial.print(wifi->getStatus());
   __status=false;
 }
-void saveConfigMqtt(){
-  EEPROM.put(0, configMqtt);
-}
 
-
-void getConfigMqtt(){
-  EEPROM.get(0, configMqtt);
-}
-
-String readSerial(){
-  String c;
-  while(Serial.available() <= 0){};
-  //while(Serial.available() > 0) c.concat((char)Serial.read());
-  c = Serial.readString();
-  c.remove(c.length()-2, 2);
-  return c;
-}
-
-void loop() {
-  if(!isConnectWifi()){ //Se o wifi estiver deconectado
-    conecte_wifi();
-  }
-  if (!client.connected()) { // Se a conexão com o broker MQtt tive caído
-    reconect();
-  }
+void verif_conf(){
+//  if(!isConnectWifi()){ //Se o wifi estiver deconectado
+//    wifi->connectWifi();
+//  }
+  
   if(__config_mode){
     config_all();
   }
   if(__status){
     status_all();
   }
-  client.loop();
-  
+  if(__reset){
+    reiniciar();
+  }
+}
+
+void acionarRele(String user){
+  client.publish((mqtt.subTopic + "/log").c_str(), ("Portão aberto por: " + user).c_str());
+  beepBuzzer(50);
+  digitalWrite(RELE, 0);
+  delay(1000);
+  digitalWrite(RELE, 1);
+}
+
+bool check_permition(String id){
+   int i;
+   if(counter_master_card >= 5) {
+    add_new_card(false);            // Adiciona um novo cartão, não master.
+    return false;
+   }
+   for(i = 0; i < VECTOR_SIZE_MAX; i++){
+    if(secure_card[i].equals(id)){
+      if(i == 0){ 
+        counter_master_card++;
+      }
+      return true; 
+    }
+   }
+   return false;
+}
+
+void add_new_card(bool master){
+  bool _add=true;
+  printMsg("Adicinar/Remover cartão");
+  buzzer(500, 3);
+  int time_out=0;
+  int i=0;
+  do {
+    tagAtual = rfid->loop();
+    time_out++;
+    if(time_out >= 40){
+      buzzer(100, 3);
+      printMsg("Cancelando...");
+      return;
+    }
+    delay(250);  
+  } while(tagAtual.equals(""));
+  if(master){                         // Se master for true ele adiciona um novo master card
+    secure_card[i] = tagAtual;
+  } else {
+    for(i=0; i<VECTOR_SIZE_MAX; i++){
+      if(secure_card[i].equals(tagAtual) && !secure_card[0].equals(tagAtual)){
+        printMsg("Ja existe esse cartão, removendo...");
+        secure_card[i] = "";
+        _add=false;
+      }
+    }
+    for (i=0; i<VECTOR_SIZE_MAX; i++){
+      if(secure_card[i].equals("")){
+        index_card = i;
+        break;
+      }
+    }
+    if(_add){
+      secure_card[index_card] = tagAtual;
+      printMsg("Um novo cartão adicionado: " + tagAtual);
+    }
+  }
+  counter_master_card=0;
+  buzzer(800, 1);
+}
+
+void loop(){
+    wifi->loop();
+    if(_look == 0){
+      tagAtual = rfid->loop();
+      if(!tagAtual.equals("")){
+        if(check_permition(tagAtual)){
+          acionarRele(tagAtual);
+          _try=0;
+        } else {
+          printMsg("Tentativa de abrir por: " + tagAtual);
+          beepBuzzer(300);
+          if(_try >= TRY_MAX){
+            printMsg("SISTEMA BLOQUEADO");
+            _look=20;
+            _try=0;
+          }
+          _try++;
+        }
+      }
+    }
+    if (!client.connected()) {
+      connect_mqtt();
+    }
+    client.loop();
+    verif_conf();
+}
+
+void printMsg(String s){
+  Serial.println(s);
+  client.publish((mqtt.subTopic + "/log").c_str(), s.c_str());
 }
